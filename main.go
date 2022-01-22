ffdb52f38a3fb93841d2e4bdd1751cc4690442d2
trevalkov <rodrigolf080@gmail.com>
2022-01-22 20:45:37 +0000

    .

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("vim-go")
+	// handle args to choose api type
+	// generate payload_list from word_list
+	// spam requests for different methods depending on api type
+	// and check which ones return a OK status code to enumerate api
+	// then format print to screen and log
 }
