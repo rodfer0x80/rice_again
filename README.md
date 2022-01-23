e6e952d49e7b694c6a49a823fd0cc1890efb1d77
trevalkov <rodrigolf080@gmail.com>
2022-01-23 21:43:06 +0000

    the_architect

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # rice_again
+## restful api
+````
+# methods
+## create
+GET
+POST
+## read
+GET
+POST
+## update
+GET
+POST
+PUT
+## delete
+GET
+POST
+DELETE
+````
+````
+## create
+/$name
+/$name/new
+/$name/create
+## read
+/$name
+## update
+/$name
+/$name/edit
+/$name/update
+## delete
+/$name
+/$name/delete
+/$name/del
+````
+## graphql
+````
+# methods
+GET
+POST
+````
+````
+# queries
+/gql?q=$name
+````
 
+## architecture
+````
+separate functions by methods
+create separate payloadlists for each method as sets so there are no duplicates
+there wordlists are made by adding each iteration of path from default/custom wordlist for resful apis
+this is for restful ofc, graphql is much easier just needs a different wordlist and can use a single payloadlist
+get status code from requests
+to build api map
+````
--- main.go
+++ main.go
@@ -1,9 +1,62 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
+
+func getArgs() (string, string) {
 	// handle args to choose api type
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: ./rice_again <domain> <api_type>")
+		os.Exit(1)
+	}
+	domain := os.Args[1]
+	apiType := os.Args[2]
+	return domain, apiType
+}
+
+func generatePayloadList(apiType string) string {
 	// generate payload_list from word_list
+	// open wordlist for this apiType
+	// read data
+	// generate wordlist for this apiType using custom data
+	// return string of payloads separated by newlines
+	payloadList := ""
+	if apiType == "graphql" || apiType == "gql" {
+		payloadList = apiType + "_graphqlPayloadList"
+	} else if apiType == "restful" || apiType == "rest" {
+		payloadList = apiType + "_restfulPayloadList"
+	} else {
+		payloadList = apiType + "_error_payloadList"
+		fmt.Println("SET 'api_type' to graphql['graphql', 'gql'] or restful['restful', 'rest]")
+
+	}
+	return payloadList
+}
+
+func flameApi(domain string, payloadList string) string {
 	// spam requests for different methods depending on api type
 	// and check which ones return a OK status code to enumerate api
+	// execute(GET/POST domain+payload) for each payload
+	// return apiMap of calls that returned OK status code
+	apiMap := payloadList + "_apiMap[" + domain + "]"
+	return apiMap
+}
+
+func formatMap(apiMap string) string {
 	// then format print to screen and log
+	output := apiMap + "_output"
+	return output
+}
+
+func main() {
+	domain, apiType := getArgs()
+
+	payloadList := generatePayloadList(apiType)
+
+	apiMap := flameApi(domain, payloadList)
+
+	output := formatMap(apiMap)
+	fmt.Printf("%s\n", output)
 }
